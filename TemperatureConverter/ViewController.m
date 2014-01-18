a1ff91c98a53909e195d1c050d34195cb43c7c34
Amber Roy <amber.roy@gmail.com>
2014-01-18 15:45:27 -0800

    Initial commit.

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -10,9 +10,43 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textFieldFahrenheit;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldCelsius;
+
 @end
 
 @implementation ViewController
+
+- (IBAction)editingDidBeginFahrenheit:(id)sender {
+    self.textFieldCelsius.text = @"";
+}
+
+- (IBAction)editingDidBeginCelsius:(id)sender {
+    self.textFieldFahrenheit.text = @"";
+}
+
+- (IBAction)tappedView:(id)sender {
+    [self.view endEditing:YES]; // Dismiss keyboard.
+}
+
+- (IBAction)convert:(id)sender {
+    [self.view endEditing:YES]; // Dismiss keyboard.
+    
+    float f_value = [self.textFieldFahrenheit.text floatValue];
+    float c_value = [self.textFieldCelsius.text floatValue];
+    
+    if (![self.textFieldFahrenheit.text isEqualToString:@""]) {
+        c_value = (5.0/9.0) * (f_value - 32);
+        self.textFieldCelsius.text = [NSString stringWithFormat:@"%2.f", c_value];
+        return;
+    }
+    
+    if (![self.textFieldCelsius.text isEqualToString:@""]) {
+        f_value = (9.0/5.0) * (c_value + 32);
+        self.textFieldFahrenheit.text = [NSString stringWithFormat:@"%2.f", f_value];
+        return;
+    }
+}
 
 - (void)viewDidLoad
 {

410280af0afde203ecb9dc362789faea94f95f11
Amber Roy <amber.roy@gmail.com>
2014-01-18 16:04:51 -0800

    Finishing touches.

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *textFieldFahrenheit;
 @property (weak, nonatomic) IBOutlet UITextField *textFieldCelsius;
+@property BOOL didConversion;
 
 @end
 
@@ -19,10 +20,12 @@
 
 - (IBAction)editingDidBeginFahrenheit:(id)sender {
     self.textFieldCelsius.text = @"";
+    self.didConversion = NO;
 }
 
 - (IBAction)editingDidBeginCelsius:(id)sender {
     self.textFieldFahrenheit.text = @"";
+    self.didConversion = NO;
 }
 
 - (IBAction)tappedView:(id)sender {
@@ -32,18 +35,24 @@
 - (IBAction)convert:(id)sender {
     [self.view endEditing:YES]; // Dismiss keyboard.
     
+    if (self.didConversion) {
+        return;
+    }
+    
     float f_value = [self.textFieldFahrenheit.text floatValue];
     float c_value = [self.textFieldCelsius.text floatValue];
     
     if (![self.textFieldFahrenheit.text isEqualToString:@""]) {
         c_value = (5.0/9.0) * (f_value - 32);
         self.textFieldCelsius.text = [NSString stringWithFormat:@"%2.f", c_value];
+        self.didConversion = YES;
         return;
     }
     
     if (![self.textFieldCelsius.text isEqualToString:@""]) {
         f_value = (9.0/5.0) * (c_value + 32);
         self.textFieldFahrenheit.text = [NSString stringWithFormat:@"%2.f", f_value];
+        self.didConversion = YES;
         return;
     }
 }

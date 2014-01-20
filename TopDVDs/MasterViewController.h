d35d7f35e9ae89d3751b8bc0cfc84b67b4518be0
Amber Roy <amber.roy@gmail.com>
2014-01-19 21:57:50 -0800

    Initial Commit

---
+++ TopDVDs/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  TopDVDs
+//
+//  Created by Amber Roy on 1/19/14.
+//  Copyright (c) 2014 Amber Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end

c3cb184372d0bc6f81c12043d390bc6b0caf5bd7
Amber Roy <amber.roy@gmail.com>
2014-01-21 09:39:48 -0800

    Initial Commit

---
+++ ToDo List/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo List
+//
+//  Created by Amber Roy on 1/21/14.
+//  Copyright (c) 2014 Amber Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDo List/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ToDo List
+//
+//  Created by Amber Roy on 1/21/14.
+//  Copyright (c) 2014 Amber Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end

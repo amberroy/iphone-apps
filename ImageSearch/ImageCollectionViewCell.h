92d12437ecd4911e75738d85ad61dcdbd3b2137c
Amber Roy <amber.roy@gmail.com>
2014-01-28 11:25:29 -0800

    Image Search using custom CollectionView with Asynchronous image loading and loading indicator.

---
+++ ImageSearch/ImageCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.h
+//  ImageSearch
+//
+//  Created by Amber Roy on 1/28/14.
+//  Copyright (c) 2014 Amber Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ ImageSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  ImageSearch
+//
+//  Created by Amber Roy on 1/28/14.
+//  Copyright (c) 2014 Amber Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController <UISearchDisplayDelegate, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
+
+@end
